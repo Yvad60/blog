2e39faf04d0eae645b92618a3c3bfccb8419123f
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2025-02-17 14:24:03 +0200

    Setup next.js with tailwindcss and typescript

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# blog
+# Blog
+
+## Getting Started
+Run the development server:
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="flex flex-col items-center mt-4">
+      <h1>Blog</h1>
+      <p>Learn and share knowledge with the world</p>
+    </main>
+  );
+};
+export default Home;
